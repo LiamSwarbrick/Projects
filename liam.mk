cd9633846c7c6e7a8b685107fb9dbbc006929750
unknown <liam.swarbrick2004@gmail.com>
2024-09-04 22:35:36 +0100

    Job files todo important

---
+++ liam.mk
@@ -0,0 +1,10 @@
+# Liam Swarbrick (Computer Science undergrad / Low level game programmer / Game Developer)
+
+I'm currently in third year at University of Leeds studying Meng/Bsc Computer Science with High Performance Computer Graphics and Games Engineering.
+
+Currently programming a voxel game in C, OpenGL 4.5, Win32API.
+
+Wrote .obj file loader to convert models to GPU ready vertex dump format.
+Wrote compiler for Java esque language 'Jack', compiling to hack computer virtual machine instructions.
+
+
