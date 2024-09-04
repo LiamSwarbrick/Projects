22b71af248dd8d6d4d3be6f61aa02369de3877ae
Liam Swarbrick <liam.swarbrick2004@gmail.com>
2024-09-05 00:05:04 +0100

    starting collecting stuff i've done in the past, but theres a lot.

--- liam.mk
+++ liam.mk
@@ -1,10 +1,19 @@
 # Liam Swarbrick (Computer Science undergrad / Low level game programmer / Game Developer)
 
+TODO: Focus on graphics programming, since focus more on depth than bredth in the indepth sections
+
 I'm currently in third year at University of Leeds studying Meng/Bsc Computer Science with High Performance Computer Graphics and Games Engineering.
 
 Currently programming a voxel game in C, OpenGL 4.5, Win32API.
 
+2D Platformer in C:
+- custom sprite animation integrated with the Aseprite pixelart program for simple fun art pipeline. (including loading collision shapes from .aseprite file).
+- integrated tilemap editor, serialising levels with json.
+
+
 Wrote .obj file loader to convert models to GPU ready vertex dump format.
 Wrote compiler for Java esque language 'Jack', compiling to hack computer virtual machine instructions.
 
+[Godot-Wild Game Jam \#5 winning game](https://itch.io/jam/godot-wild-jam-5/rate/358355)
 
+Heres a Nintendo Switch port of my game by Ariel Mansour (creator of Godot, alongside Juan Linietsky), it was all thanks to Ariel Mansour who thought it was cool and emailed 
